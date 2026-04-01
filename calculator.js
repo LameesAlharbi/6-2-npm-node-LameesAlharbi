b2a6342a231bc32f5ee2f4446ce0a3d25aa9cbbc
LameesAlharbi <harbilamees86@gmail.com>
2026-04-01 12:54:15 +0300

    todo 5

--- calculator.js
+++ calculator.js
@@ -267,3 +267,15 @@ export function divide(numbers) {
     return result / num;
   }, 0);
 }
+
+import _ from "lodash";
+
+export function parseNumbers(input) {
+  const numbers = _.map(input, (str) => Number(str));
+  return _.compact(numbers);
+}
+
+export function isValidOperation(operation) {
+  const validOps = ["add", "subtract", "multiply", "divide"];
+  return _.includes(validOps, operation);
+}
